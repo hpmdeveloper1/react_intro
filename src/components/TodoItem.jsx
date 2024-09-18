b62fd3d942a7882f0053bb409ce3d12d61629a06
Francisco Cervantes <developer1@highpower.com.mx>
2024-09-18 12:25:40 -0600

    se agregan iconos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,24 +1,34 @@
+import { AiFillCheckCircle } from "react-icons/ai";
+import { MdDeleteForever } from "react-icons/md";
 import ('../assets/css/todoItem.css')
 
 
 function TodoItem({ text, completed, onComplete, onDelete }){
     return (
       <li className="todoItem">
-        <span 
+        <AiFillCheckCircle 
+          className={`icon icon-check ${completed && "icon-check-active"}`}
+          onClick={onComplete}  
+        />
+        {/* <span 
           className={`icon icon-check ${completed && "icon-check-active"}`}
           onClick={onComplete}
         >
           V
-        </span>
+        </span> */}
         <p className={`todoItem-text ${completed && "todoItem-text-complete"}`}>
           {text}
         </p>
-        <span 
+        <MdDeleteForever 
+          className="icon icon-delete"
+          onClick={onDelete}
+        />
+        {/* <span 
           className="icon icon-delete"
           onClick={onDelete}
         >
           X
-        </span>
+        </span> */}
       </li>
     )
 }
